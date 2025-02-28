cde7cc6d20c3aa66e6871d0ac23ddf9db0a3e276
N1KFY <niky@hotmail.com>
2025-02-28 12:19:08 +0100

    Co-authored-by: miquiggik <miquiggik@users.noreply.github.com>

---
+++ database/migrations/2025_02_25_143735_create_reservas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reserves', function (Blueprint $table) {
+            $table->id();
+            $table->date('data_inici');
+            $table->date('data_fi');
+            $table->string('email_usuari');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reserves');
+    }
+};
